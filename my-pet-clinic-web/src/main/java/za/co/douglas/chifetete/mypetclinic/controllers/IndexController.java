c6e0fdeb2b4e0ff65f68bcc2c984a27a7f614b8c
Douglas Shingirayi Chifetete <douglas.chifetete@gmail.com>
2019-01-08 12:13:13 +0200

    Added index page and controller mock. Closes #8

---
+++ my-pet-clinic-web/src/main/java/za/co/douglas/chifetete/mypetclinic/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package za.co.douglas.chifetete.mypetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"/","","index","index.html"})
+    public String index() {
+        return "index";
+    }
+
+}
